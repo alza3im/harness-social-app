20ac7e626f7433c7c0d04ffc06cc80c95a57c1a1
Khalil <khalil@Khalils-MacBook-Air.local>
2023-06-15 23:02:53 -0300

    created login and signup views

--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.views import APIView
+from .serializer import UserSerializer
+from .models import User
 
-# Create your views here.
+
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data["email"]
+        password = request.data["password"]
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed("user does not exist")
+
+        if not user.check_password(password):
+            raise AuthenticationFailed("Incorrect password")
+
+        return Response({"message": "Success !! You are now logged in"})
