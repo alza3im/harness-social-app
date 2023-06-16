6844549ba9b21711e7157ae13a072b2f55d7a209
Khalil <khalil@Khalils-MacBook-Air.local>
2023-06-16 01:50:07 -0300

    Post Crud w/ like post

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
 
-from .views import PostCreateView
+from .views import (
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    PostDetailView,
+    PostLikeView,
+)
 
 urlpatterns = [
     path("new/", PostCreateView.as_view(), name="post-create"),
+    path("<int:post_id>/update/", PostUpdateView.as_view(), name="post-update"),
+    path(
+        "<int:post_id>/delete/",
+        PostDeleteView.as_view(),
+        name="post-delete",
+    ),
+    path("<int:post_id>/", PostDetailView.as_view(), name="post-detail"),
+    path("<int:post_id>/like/", PostLikeView.as_view(), name="post-like"),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .serializer import PostSerializer
+from .models import Post
 
 
 class PostCreateView(APIView):
@@ -18,3 +20,49 @@ class PostCreateView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class PostUpdateView(APIView):
+    def put(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        serializer = PostSerializer(
+            instance=post,
+            data=request.data,
+            context={"request": request},
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class PostDeleteView(APIView):
+    def delete(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class PostDetailView(APIView):
+    def get(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+
+class PostLikeView(APIView):
+    def post(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        user = request.user
+
+        if user in post.likes.all():
+            # User has already liked the post, remove the like
+            post.likes.remove(user)
+        else:
+            # User has not liked the post, add the like
+            post.likes.add(user)
+
+        # Update the number of likes
+        num_likes = post.likes.count()
+
+        return Response({"likes": num_likes})
