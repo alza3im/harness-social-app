80769a2ec232c51d4de2431bd69546bc722f5b7b
Khalil <khalil@Khalils-MacBook-Air.local>
2023-06-16 17:48:09 -0300

    tests for SignUpView

--- src/user/tests.py
+++ src/user/tests.py
@@ -1,3 +1,63 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+from .models import User, UserGeolocation
 
-# Create your tests here.
+
+class SignUpViewTestCase(TestCase):
+    """
+    Test case for the SignUpView.
+    """
+
+    def setUp(self):
+        """
+        Set up the test client.
+        """
+        self.client = APIClient()
+
+    def test_signup_success(self):
+        """
+        Test successful user signup.
+        """
+        url = reverse("signup")
+        data = {
+            "name": "John Doe",
+            "email": "johndoe@example.com",
+            "password": "password123",
+        }
+
+        response = self.client.post(url, data, format="json")
+
+        # Assert the response status code is HTTP 200 OK
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Assert a user object has been created
+        self.assertEqual(User.objects.count(), 1)
+
+        # Get the created user object
+        user = User.objects.first()
+
+        # Assert user attributes match the provided data
+        self.assertEqual(user.name, data["name"])
+        self.assertEqual(user.email, data["email"])
+        self.assertTrue(user.check_password(data["password"]))
+
+    def test_signup_missing_name_data(self):
+        """
+        Test signup with missing name data.
+        """
+        url = reverse("signup")
+        data = {
+            "email": "invalid_email",
+            "password": "password123",
+        }
+
+        response = self.client.post(url, data, format="json")
+
+        # Assert the response status code is HTTP 400 Bad Request
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Assert no user or user geolocation objects have been created
+        self.assertEqual(User.objects.count(), 0)
+        self.assertEqual(UserGeolocation.objects.count(), 0)
--- src/user/urls.py
+++ src/user/urls.py
@@ -4,8 +4,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import SignUpView, LoginView
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view()),
-    path("login/", LoginView.as_view()),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
